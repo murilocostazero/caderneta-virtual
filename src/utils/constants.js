a3423970996f7f1507704e98284e067024502224
Sergio Murilo <murilocostazero@gmail.com>
2026-09-02 15:57:30 -0300

    Mudando o ip do backend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'https://caderneta-virtual.up.railway.app';
-// export const BASE_URL = 'https://caderneta-virtual-backend-bynwc.ondigitalocean.app';
-export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://caderneta-virtual-backend-bynwc.ondigitalocean.app';
+// export const BASE_URL = 'http://localhost:3000'
